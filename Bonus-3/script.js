2855269e27d707d0ccfabbaef47d4584975fa05a
Raff_12 <raffelecoppola381@gmail.com>
2025-03-04 19:12:30 +0100

    feat: Modifica di stile al progetto con animazione

--- Bonus-3/script.js
+++ Bonus-3/script.js
@@ -1,18 +1,20 @@
 //Bonus-3: Creazione dello switch nella versione 2.0
 
 //Creazione delle seguenti variabili
+
 let btn_switch = document.getElementById('btn-switch');
 let text = document.querySelectorAll('.text-switch');
 let box_light = document.querySelector('.body-bg.light');
 let box_dark = document.querySelector('.body-bg.dark');
 let switch_line = document.querySelector('.btn-box');
 
-//funzione di creazione dello switch
+//Funzione di interazione sul tipo di evento
 
 function SwitchHandle(){
     if (!btn_switch.classList.contains('dark')) {
         btn_switch.classList.add('dark');
         switch_line.classList.add('dark');
+        switch_line.classList.remove('light');
         for (let index = 0; index < text.length; index++) {
             text[index].classList.add('dark');
         }
@@ -24,6 +26,7 @@ function SwitchHandle(){
         btn_switch.classList.remove('dark');
         box_dark.classList.add('missing');
         box_light.classList.remove('missing');
+        switch_line.classList.add('light');
         switch_line.classList.remove('dark');
         for (let index = 0; index < text.length; index++) {
             text[index].classList.remove('dark');
@@ -31,4 +34,5 @@ function SwitchHandle(){
     }
 }
 
+//Aggiunta del tipo di evento al bottone
 btn_switch.addEventListener('click', SwitchHandle);
